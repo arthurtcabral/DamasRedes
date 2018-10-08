46732b2f88b311d1a20ac72a9cbcf5e50df33c3c
Arthur Tassinari Cabral <Arthur Tassinari Cabral>
2018-10-07 22:06:48 -0300

    Revert "Subir lógica de montagem do tabuleiro para um jogador e demais logisticas de leitura."
    This reverts commit 473bf990706e9f3fac826dcf3887c7714307f8a2.

--- src/main/java/br/com/unisinos/damasredes/Servidor.java
+++ src/main/java/br/com/unisinos/damasredes/Servidor.java
@@ -1,64 +1,54 @@
 package br.com.unisinos.damasredes;
 
+import lombok.extern.log4j.Log4j;
+
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class Servidor {
 
-	public static void main(String[] args) throws ClassNotFoundException, IOException {
-		Servidor servidor = new Servidor();
+    public static void main(String[] args) {
+        Servidor servidor = new Servidor();
 
-		ObjectInputStream doCliente = new ObjectInputStream(
-				servidor.clientA.getInputStream());
+        servidor.closeConnections();
+    }
 
-		Cliente jogadorA = (Cliente) doCliente.readObject();
-		System.out.println(jogadorA.getPosicaoDeOrigem()[0]);
-		System.out.println(jogadorA.getPosicaoDeOrigem()[1]);
+    private Socket clientA;
+    private Socket clientB;
 
-		servidor.closeConnections();
-	}
+    public Servidor() {
+        start();
+    }
 
-	private Socket clientA;
-	private Socket clientB;
+    private void start() {
+        try {
+            ServerSocket socketClientA = new ServerSocket(getPort("client.a.port"));
+            ServerSocket socketClientB = new ServerSocket(getPort("client.b.port"));
 
-	public Servidor() throws ClassNotFoundException {
-		start();
-	}
+            System.out.println("Aguardando conexão do primeiro jogador...");
+            clientA = socketClientA.accept();
 
-	private void start() throws ClassNotFoundException {
-		try {
-			ServerSocket socketClientA = new ServerSocket(
-					getPort("client.a.port"));
-			ServerSocket socketClientB = new ServerSocket(
-					getPort("client.b.port"));
+            System.out.println("Aguardando conexão do segundo jogador...");
+            clientB = socketClientB.accept();
+        } catch (IOException e) {
+            log.error("Problema na criação dos sockets do servidor", e);
+        }
+    }
 
-			System.out.println("Aguardando conexão do primeiro jogador...");
-			clientA = socketClientA.accept();
+    public void closeConnections() {
+        System.out.println("Fechando conexões com clients.");
+        try {
+            clientA.close();
+            clientB.close();
+        } catch (IOException e) {
+            log.error("Erro no fechamento dos sockets", e);
+        }
+    }
 
-			clientB = socketClientB.accept();
-
-		} catch (IOException e) {
-			// log.error("Problema na criação dos sockets do servidor", e);
-		}
-	}
-
-	public void closeConnections() {
-		System.out.println("Fechando conexões com clients.");
-		try {
-			clientA.close();
-			clientB.close();
-		} catch (IOException e) {
-			// log.error("Erro no fechamento dos sockets", e);
-		}
-	}
-
-	private int getPort(String s) {
-		String port = GameProps.getForServer().getProperty(s);
-		return Integer.parseInt(port);
-	}
+    private int getPort(String s) {
+        String port = GameProps.getForServer().getProperty(s);
+        return Integer.parseInt(port);
+    }
 }
