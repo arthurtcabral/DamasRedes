5ff3bdd480dc35b604db7bf381b5d21d00a5779e
Felipe Souza <felipe.souza@cwi.com.br>
2018-10-08 20:36:53 -0300

    Reposicionar lógica de jogo no Servidor

--- src/main/java/br/com/unisinos/damasredes/Cliente.java
+++ src/main/java/br/com/unisinos/damasredes/Cliente.java
@@ -1,138 +1,95 @@
 package br.com.unisinos.damasredes;
 
-import java.io.BufferedReader;
-import br.com.unisinos.damasredes.frontend.ScreenHelper;
 import br.com.unisinos.damasredes.mensagem.MensagemCliente;
 import br.com.unisinos.damasredes.mensagem.MensagemServidor;
 import br.com.unisinos.damasredes.mensagem.Status;
 import br.com.unisinos.damasredes.mensagem.Tipo;
+import lombok.Getter;
 import lombok.extern.log4j.Log4j;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-import lombok.extern.log4j.Log4j;
-
-import static br.com.unisinos.damasredes.frontend.ScreenHelper.aguardarJogada;
-import static br.com.unisinos.damasredes.frontend.ScreenHelper.notificarTurnoAdversario;
-import static br.com.unisinos.damasredes.frontend.ScreenHelper.printGameScreen;
+import static br.com.unisinos.damasredes.frontend.ScreenHelper.*;
 
 @Log4j
+@Getter
 public class Cliente {
 
-	private String tabuleiro[][];
-	private int[] posicaoDeOrigem;
-	private int[] posicaoDeDestino;
-	private int numeroDePecas;
-	private EStatus status;
-	private Socket server;
-	
-	public static void main(String[] args) throws IOException {
-		Cliente servidor = new Cliente();
-		ObjectOutputStream paraServidor = new ObjectOutputStream(
-				servidor.server.getOutputStream());
-		BufferedReader doServidor = new BufferedReader(new InputStreamReader(
-				servidor.server.getInputStream()));
-		servidor.jogar(doServidor);
-		paraServidor.writeObject(servidor);
-	}
-	
-	public Cliente() throws IOException {
-		start();
-	}
+    private int[] posicaoDeOrigem;
+    private int[] posicaoDeDestino;
+    private Socket server;
 
-	private void start() throws IOException {
-		server = estabelecerConexao();
-		System.out.println("Conexão estabelecida.");
-		
-	}
+    public static void main(String[] args) {
+        Cliente servidor = new Cliente();
 
-	private Socket estabelecerConexao() {
-		try {
-			this.status = EStatus.JOGANDO;
-			this.numeroDePecas = 12;
-			this.tabuleiro = new String[8][8];
-			for (int i = 0; i < tabuleiro.length; i++) {
-				for (int j = 0; j < tabuleiro.length; j++) {
-					this.tabuleiro[i][j] = "V";
-				}
-			}
-			for (int i = 0; i < 4; i++) {
-				int coluna = i % 2 == 0 ? 0 : 1;
-				for (int j = coluna; j < tabuleiro.length; j = j + 2) {
-					this.tabuleiro[i][j] = "X";
-				}
-			}
-			System.out.println("Tentando estabelecer conexão com servidor...");
-			return new Socket(GameProps.getForClient().getProperty(
-					"server.host"), getPort("server.port"));
-		} catch (IOException e) {
-			// log.error("Problema na conexão ao servidor", e);
-			System.out
-					.println("Não foi possível estabelecer conexão com o servidor.");
-			throw new RuntimeException(
-					"Não foi possível estabelecer conexão com o servidor.");
-		}
-	}
+        while (true) {
+            MensagemServidor mensagem = servidor.waitForMessage();
+            if (mensagem.isGameOver()) {
+                servidor.endGame(mensagem);
+                break;
+            }
 
-	private int getPort(String s) {
-		String port = GameProps.getForClient().getProperty(s);
-		return Integer.parseInt(port);
-	}
+            printGameScreen(mensagem);
+            if (mensagem.getTipo() == Tipo.INFORMACAO) {
+                notificarTurnoAdversario();
+            } else {
+                MensagemCliente jogada = aguardarJogada();
+            }
+        }
+    }
 
-	private void jogar(BufferedReader doServidor) {
-		// TODO: Apresentar tabuleiro.
-		// TODO: Ler informa��es oriundas da l�gica de jogo do servidor
-		Scanner teclado = new Scanner(System.in);
-		System.out.println("Informe a linha e a coluna da posi��o de origem: ");
-		this.posicaoDeOrigem[0] = teclado.nextInt();
-		this.posicaoDeOrigem[1] = teclado.nextInt();
-		
-		System.out.println("Informe a linha e a coluna da posi��o de destino: ");
-		this.posicaoDeDestino[0] = teclado.nextInt();
-		this.posicaoDeDestino[1] = teclado.nextInt();
-	}
+    private Cliente() {
+        start();
+    }
 
-	public String[][] getTabuleiro() {
-		return tabuleiro;
-	}
+    private void start() {
+        server = estabelecerConexao();
+        System.out.println("Conexão estabelecida.");
+    }
 
-	public void setTabuleiro(String[][] tabuleiro) {
-		this.tabuleiro = tabuleiro;
-	}
+    private MensagemServidor waitForMessage() {
+        return null;
+    }
 
-	public int[] getPosicaoDeOrigem() {
-		return this.posicaoDeOrigem;
-	}
+    private void endGame(MensagemServidor mensagem) {
+        if (mensagem.getStatus() == Status.GANHOU) {
 
-	public void setPosicaoDeOrigem(int[] posicaoDeOrigem) {
-		this.posicaoDeOrigem = posicaoDeOrigem;
-	}
+        } else {
 
-	public int[] getPosicaoDeDestino() {
-		return this.posicaoDeDestino;
-	}
+        }
+    }
 
-	public void setPosicaoDeDestino(int[] posicaoDeDestino) {
-		this.posicaoDeDestino = posicaoDeDestino;
-	}
+    private Socket estabelecerConexao() {
+        try {
+            System.out.println("Tentando estabelecer conexão com servidor...");
+            return new Socket(GameProps.getForClient().getProperty(
+                    "server.host"), getPort("server.port"));
+        } catch (IOException e) {
+            log.error("Problema na conexão ao servidor", e);
+            System.out.println("Não foi possível estabelecer conexão com o servidor.");
+            throw new RuntimeException(
+                    "Não foi possível estabelecer conexão com o servidor.");
+        }
+    }
 
-	public int getNumeroDePecas() {
-		return numeroDePecas;
-	}
+    private int getPort(String s) {
+        String port = GameProps.getForClient().getProperty(s);
+        return Integer.parseInt(port);
+    }
 
-	public void setNumeroDePecas(int numeroDePecas) {
-		this.numeroDePecas = numeroDePecas;
-	}
+    private void jogar(BufferedReader doServidor) {
+        // TODO: Apresentar tabuleiro.
+        // TODO: Ler informa��es oriundas da l�gica de jogo do servidor
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Informe a linha e a coluna da posi��o de origem: ");
+        this.posicaoDeOrigem[0] = teclado.nextInt();
+        this.posicaoDeOrigem[1] = teclado.nextInt();
 
-	public EStatus getStatus() {
-		return status;
-	}
-
-	public void setStatus(EStatus status) {
-		this.status = status;
-	}
+        System.out.println("Informe a linha e a coluna da posi��o de destino: ");
+        this.posicaoDeDestino[0] = teclado.nextInt();
+        this.posicaoDeDestino[1] = teclado.nextInt();
+    }
 }
--- src/main/java/br/com/unisinos/damasredes/Servidor.java
+++ src/main/java/br/com/unisinos/damasredes/Servidor.java
@@ -1,64 +1,88 @@
 package br.com.unisinos.damasredes;
 
+import br.com.unisinos.damasredes.mensagem.Status;
+import lombok.extern.log4j.Log4j;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import lombok.extern.log4j.Log4j;
-
 @Log4j
 public class Servidor {
 
-	public static void main(String[] args) throws ClassNotFoundException, IOException {
-		Servidor servidor = new Servidor();
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
+        Servidor servidor = new Servidor();
 
-		ObjectInputStream doCliente = new ObjectInputStream(
-				servidor.clientA.getInputStream());
+        ObjectInputStream doCliente = new ObjectInputStream(
+                servidor.clientA.getInputStream());
 
-		Cliente jogadorA = (Cliente) doCliente.readObject();
-		System.out.println(jogadorA.getPosicaoDeOrigem()[0]);
-		System.out.println(jogadorA.getPosicaoDeOrigem()[1]);
+        Cliente jogadorA = (Cliente) doCliente.readObject();
+        System.out.println(jogadorA.getPosicaoDeOrigem()[0]);
+        System.out.println(jogadorA.getPosicaoDeOrigem()[1]);
 
-		servidor.closeConnections();
-	}
+        servidor.closeConnections();
+    }
 
-	private Socket clientA;
-	private Socket clientB;
+    private Socket clientA;
+    private Socket clientB;
+    private int numeroDePecas;
+    private Status status;
+    private Socket server;
+    private String tabuleiro[][];
 
-	public Servidor() throws ClassNotFoundException {
-		start();
-	}
+    public Servidor() {
+        start();
+    }
 
-	private void start() throws ClassNotFoundException {
-		try {
-			ServerSocket socketClientA = new ServerSocket(
-					getPort("client.a.port"));
-			ServerSocket socketClientB = new ServerSocket(
-					getPort("client.b.port"));
+    private void start() {
+        try {
+            ServerSocket socketClientA = new ServerSocket(
+                    getPort("client.a.port"));
+            ServerSocket socketClientB = new ServerSocket(
+                    getPort("client.b.port"));
 
-			System.out.println("Aguardando conexão do primeiro jogador...");
-			clientA = socketClientA.accept();
+            System.out.println("Aguardando conexão do primeiro jogador...");
+            clientA = socketClientA.accept();
 
-			clientB = socketClientB.accept();
+            System.out.println("Aguardando conexão do segundo jogador...");
+            clientB = socketClientB.accept();
 
-		} catch (IOException e) {
-			// log.error("Problema na criação dos sockets do servidor", e);
-		}
-	}
+            System.out.println("\nIniciando jogo!");
+        } catch (IOException e) {
+            log.error("Problema na criação dos sockets do servidor", e);
+        }
+    }
 
-	public void closeConnections() {
-		System.out.println("Fechando conexões com clients.");
-		try {
-			clientA.close();
-			clientB.close();
-		} catch (IOException e) {
-			// log.error("Erro no fechamento dos sockets", e);
-		}
-	}
+    private void initializeData() {
+        this.status = Status.JOGANDO;
+        this.numeroDePecas = 12;
+        this.tabuleiro = new String[8][8];
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro.length; j++) {
+                this.tabuleiro[i][j] = "V";
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            int coluna = i % 2 == 0 ? 0 : 1;
+            for (int j = coluna; j < tabuleiro.length; j = j + 2) {
+                this.tabuleiro[i][j] = "X";
+            }
+        }
+    }
 
-	private int getPort(String s) {
-		String port = GameProps.getForServer().getProperty(s);
-		return Integer.parseInt(port);
-	}
+    public void closeConnections() {
+        System.out.println("Fechando conexões com clients.");
+        try {
+            clientA.close();
+            clientB.close();
+        } catch (IOException e) {
+            log.error("Erro no fechamento dos sockets", e);
+        }
+    }
+
+    private int getPort(String s) {
+        String port = GameProps.getForServer().getProperty(s);
+        return Integer.parseInt(port);
+    }
 }
