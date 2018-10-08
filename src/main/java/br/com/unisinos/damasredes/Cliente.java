54e0b2502f4b8da42dff48e1ceac0889fc28f112
Arthur Tassinari Cabral <Arthur Tassinari Cabral>
2018-10-08 20:16:35 -0300

    Revert "Revert "Subir lógica de montagem do tabuleiro para um jogador e demais logisticas de leitura.""
    This reverts commit 46732b2f88b311d1a20ac72a9cbcf5e50df33c3c.

--- src/main/java/br/com/unisinos/damasredes/Cliente.java
+++ src/main/java/br/com/unisinos/damasredes/Cliente.java
@@ -1,41 +1,127 @@
 package br.com.unisinos.damasredes;
 
-import lombok.extern.log4j.Log4j;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
+
+import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class Cliente {
 
-    public static void main(String[] args) {
-        Cliente servidor = new Cliente();
-    }
+	private String tabuleiro[][];
+	private int[] posicaoDeOrigem;
+	private int[] posicaoDeDestino;
+	private int numeroDePecas;
+	private EStatus status;
+	private Socket server;
+	
+	public static void main(String[] args) throws IOException {
+		Cliente servidor = new Cliente();
+		ObjectOutputStream paraServidor = new ObjectOutputStream(
+				servidor.server.getOutputStream());
+		BufferedReader doServidor = new BufferedReader(new InputStreamReader(
+				servidor.server.getInputStream()));
+		servidor.jogar(doServidor);
+		paraServidor.writeObject(servidor);
+	}
+	
+	public Cliente() throws IOException {
+		start();
+	}
 
-    private Socket server;
+	private void start() throws IOException {
+		server = estabelecerConexao();
+		System.out.println("Conexão estabelecida.");
+		
+	}
 
-    public Cliente() {
-        start();
-    }
+	private Socket estabelecerConexao() {
+		try {
+			this.status = EStatus.JOGANDO;
+			this.numeroDePecas = 12;
+			this.tabuleiro = new String[8][8];
+			for (int i = 0; i < tabuleiro.length; i++) {
+				for (int j = 0; j < tabuleiro.length; j++) {
+					this.tabuleiro[i][j] = "V";
+				}
+			}
+			for (int i = 0; i < 4; i++) {
+				int coluna = i % 2 == 0 ? 0 : 1;
+				for (int j = coluna; j < tabuleiro.length; j = j + 2) {
+					this.tabuleiro[i][j] = "X";
+				}
+			}
+			System.out.println("Tentando estabelecer conexão com servidor...");
+			return new Socket(GameProps.getForClient().getProperty(
+					"server.host"), getPort("server.port"));
+		} catch (IOException e) {
+			// log.error("Problema na conexão ao servidor", e);
+			System.out
+					.println("Não foi possível estabelecer conexão com o servidor.");
+			throw new RuntimeException(
+					"Não foi possível estabelecer conexão com o servidor.");
+		}
+	}
 
-    private void start() {
-        server = estabelecerConexao();
-        System.out.println("Conexão estabelecida.");
-    }
+	private int getPort(String s) {
+		String port = GameProps.getForClient().getProperty(s);
+		return Integer.parseInt(port);
+	}
 
-    private Socket estabelecerConexao() {
-        try {
-            System.out.println("Tentando estabelecer conexão com servidor...");
-            return new Socket(GameProps.getForClient().getProperty("server.host"), getPort("server.port"));
-        } catch (IOException e) {
-            log.error("Problema na conexão ao servidor", e);
-            System.out.println("Não foi possível estabelecer conexão com o servidor.");
-            throw new RuntimeException("Não foi possível estabelecer conexão com o servidor.");
-        }
-    }
+	private void jogar(BufferedReader doServidor) {
+		// TODO: Apresentar tabuleiro.
+		// TODO: Ler informa��es oriundas da l�gica de jogo do servidor
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Informe a linha e a coluna da posi��o de origem: ");
+		this.posicaoDeOrigem[0] = teclado.nextInt();
+		this.posicaoDeOrigem[1] = teclado.nextInt();
+		
+		System.out.println("Informe a linha e a coluna da posi��o de destino: ");
+		this.posicaoDeDestino[0] = teclado.nextInt();
+		this.posicaoDeDestino[1] = teclado.nextInt();
+	}
 
-    private int getPort(String s) {
-        String port = GameProps.getForClient().getProperty(s);
-        return Integer.parseInt(port);
-    }
+	public String[][] getTabuleiro() {
+		return tabuleiro;
+	}
+
+	public void setTabuleiro(String[][] tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+
+	public int[] getPosicaoDeOrigem() {
+		return this.posicaoDeOrigem;
+	}
+
+	public void setPosicaoDeOrigem(int[] posicaoDeOrigem) {
+		this.posicaoDeOrigem = posicaoDeOrigem;
+	}
+
+	public int[] getPosicaoDeDestino() {
+		return this.posicaoDeDestino;
+	}
+
+	public void setPosicaoDeDestino(int[] posicaoDeDestino) {
+		this.posicaoDeDestino = posicaoDeDestino;
+	}
+
+	public int getNumeroDePecas() {
+		return numeroDePecas;
+	}
+
+	public void setNumeroDePecas(int numeroDePecas) {
+		this.numeroDePecas = numeroDePecas;
+	}
+
+	public EStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(EStatus status) {
+		this.status = status;
+	}
 }
