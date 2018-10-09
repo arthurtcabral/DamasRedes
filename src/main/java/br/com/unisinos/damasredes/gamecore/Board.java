7cd24a322c9882092d294e3ada5e8522efbc56e9
Leonardo Broch de Morais <leo_broch@hotmail.com>
2018-10-09 15:01:26 -0300

    DamasRedes

--- src/main/java/br/com/unisinos/damasredes/gamecore/Board.java
+++ src/main/java/br/com/unisinos/damasredes/gamecore/Board.java
@@ -31,13 +31,42 @@ public class Board {
 		return board;
 	}
 
-	private boolean canMove(Point startPosition, Point endPosition) {
-		if ((endPosition.x == startPosition.x + 1 || endPosition.x == startPosition.x -1)
-				&& (endPosition.y == startPosition.y + 1 || endPosition.y == startPosition.y -1)) {
-			return true;
+	private boolean canMove(int turn, Point startPosition, Point endPosition) {
+		if ((turn == 1 && endPosition.y > startPosition.y) !! (turn == 2 && endPosition.y < startPosition.y)) {
+			return false;
 		}
-		if ()
-		return false;
+		if (!(endPosition.x == startPosition.x + 1 || endPosition.x == startPosition.x -1)
+				|| !(endPosition.y == startPosition.y + 1 || endPosition.y == startPosition.y -1)) {
+			return false;
+		}
+		if (board[endPosition.y][endPosition.x] != EMPTY) {
+			return false;
+		}
+		if (board[startPosition.y][startPosition.x] != turn) {
+			return false;
+		}
+		return true;
 	}
 
+	public void play(int turn, Point startPosition, Point endPosition) {
+		if (canMove(turn, startPosition, endPosition) ) {
+			board[startPosition.y][startPosition.x] = EMPTY;
+			board[endPosition.y][endPosition.x] = turn;
+		}
+	}
+
+	public void print() {
+		for (int i = z0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+	}
+
+	public static void main(String[] args) {
+		Board board = new Board();
+		board.play(BLACK, new Point(0, 2), new Point(2, 3));
+		board.print();
+	}
 }
